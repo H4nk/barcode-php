5e8707c37fa9cda3720dae6f4166defd4cef9798
Harry Setya Hadi <xmoensen@gmail.com>
2022-12-04 19:12:03 +0700

    Create index.php
    https://sih4nk.com/pemograman/php/cara-membuat-barcode-dengan-php.html

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Barcode Generator 128</h1>
+    <form action="proses.php">
+        <label for="data">Data</label>
+        <input type="text" id="data" name="data"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
